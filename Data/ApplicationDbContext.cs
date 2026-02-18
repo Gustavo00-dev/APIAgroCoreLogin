7dcd1bedd074e79b1932b5b97ecbb21b8869cc1d
Gustavo Rodrigues <gustavorodrigues955@gmail.com>
2026-02-18 20:27:09 -0300

    feat: banco

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace APIAgroCoreLogin.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+
         }
 
         public DbSet<Usuario> Usuarios { get; set; }
